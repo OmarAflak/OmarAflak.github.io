5ea4f5496d4e97f41bb822ac8dde52149940ffe8
Omar Aflak <aflakomar@gmail.com>
2022-10-16 21:00:03 +0100

    update all then draw

--- sketches/gravity/gravity.js
+++ sketches/gravity/gravity.js
@@ -1,65 +1,67 @@
 const magnitude = (x, y) => Math.sqrt(x * x + y * y)
 
-class Particle {
-    constructor(x, y, vx, vy) {
-        this.x = x
-        this.y = y
-        this.vx = vx
-        this.vy = vy
-        this.color = random_rgba(1)
-        this.size = 3
-    }
+create_particles = (total) => Array.from(Array(total), create_particle)
 
-    update(particles, min_distance) {
-        let ax = 0
-        let ay = 0
-
-        for (const p of particles) {
-            if (p == this) {
-                continue;
-            }
-
-            const dx = p.x - this.x
-            const dy = p.y - this.y
-            const distance = dx * dx + dy * dy
-            let g = Math.sqrt(distance) < min_distance ? -1 : 1
-
-            ax += g * dx / distance
-            ay += g * dy / distance
-        }
-
-        const l = magnitude(ax, ay)
-        if (l > 0.2) {
-            ax *= 0.2 / l
-            ay *= 0.2 / l
-        }
-        
-        this.vx += ax
-        this.vy += ay
-
-        const ll = magnitude(this.vx, this.vy)
-        if (ll > 4) {
-            this.vx *= 4 / ll
-            this.vy *= 4 / ll
-        }
-
-        this.x += this.vx
-        this.y += this.vy
-
-        if (this.x <= 0 || this.x + this.size >= WIDTH) {
-            this.vx *= -1
-        }
-        if (this.y <= 0 || this.y + this.size >= HEIGHT) {
-            this.vy *= -1
-        }
-    }
-
-    draw() {
-        draw_rectangle(this.x, this.y, this.size, this.size, this.color)
+create_particle = () => {
+    return {
+        "x": Math.random() * WIDTH,
+        "y": Math.random() * HEIGHT,
+        "vx": Math.random() - 0.5,
+        "vy": Math.random() - 0.5,
+        "size": 3,
+        "color": random_rgba(1)
     }
 }
 
-const create_particles = (total) => Array.from(Array(total), () => new Particle(WIDTH * Math.random(), HEIGHT * Math.random(), 0, 0))
+update_particle = (p, particles, min_distance) => {
+    const copy = Object.assign({}, p)
+
+    let ax = 0
+    let ay = 0
+
+    for (const other of particles) {
+        if (other == p) {
+            continue;
+        }
+
+        const dx = other.x - copy.x
+        const dy = other.y - copy.y
+        const d = dx * dx + dy * dy
+        const g = Math.sqrt(d) < min_distance ? -1 : 1
+
+        ax += g * dx / d
+        ay += g * dy / d
+    }
+
+    const a = magnitude(ax, ay)
+    if (a > 0.2) {
+        ax *= 0.2 / a
+        ay *= 0.2 / a
+    }
+    
+    copy.vx += ax
+    copy.vy += ay
+
+    const v = magnitude(copy.vx, copy.vy)
+    if (v > 4) {
+        copy.vx *= 4 / v
+        copy.vy *= 4 / v
+    }
+
+    copy.x += copy.vx
+    copy.y += copy.vy
+
+    if (copy.x <= 0 || copy.x + copy.size >= WIDTH) {
+        copy.vx *= -1
+    }
+    if (copy.y <= 0 || copy.y + copy.size >= HEIGHT) {
+        copy.vy *= -1
+    }
+
+    return copy
+}
+
+draw_particle = (p) => draw_rectangle(p.x, p.y, p.size, p.size, p.color)
 
 const total_particles_slider = document.getElementById("total_particles")
 const total_particles_placeholder = document.getElementById("total_particles_placeholder")
@@ -85,11 +87,9 @@ let particles = create_particles(total_particles)
 
 const update = () => {
     clear_canvas()
-    
-    for (const p of particles) {
-        p.update(particles, min_distance)
-        p.draw()
-    }
+
+    particles = particles.map(p => update_particle(p, particles, min_distance))
+    particles.forEach(draw_particle)
 
     if (should_recreate_particles) {
         should_recreate_particles = false
