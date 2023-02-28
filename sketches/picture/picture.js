9c852be42a2cb04e367219d3feeddb36dc1fa48f
Omar Aflak <aflakomar@gmail.com>
2023-02-28 21:30:58 +0000

    two sources

--- sketches/picture/picture.js
+++ sketches/picture/picture.js
@@ -1,27 +1,50 @@
 resize_canvas(1000, 1000)
 
 const GAP = 50
-const X0 = GAP
-const Y0 = HEIGHT - GAP
 const G = 1
 const TIME = 60
 const DELTA_TIME = 0.1
 
-const compute_velocity = (target_t, target_x, target_y) => {
-    const time = TIME - target_t
-    const v0x = (target_x - X0) / time
-    const v0y = (target_y - Y0 - 0.5 * G * time * time) / time
+const compute_velocity = (start, end, target_time) => {
+    const time = TIME - target_time
+    const v0x = (end.x - start.x) / time
+    const v0y = (end.y - start.y - 0.5 * G * time * time) / time
     return create_point(v0x, v0y)
 }
 
-const update_point = (time, point, velocity) => {
-    if (point.y > HEIGHT - 10) {
+const update_particle = (particle, time) => {
+    if (particle.position.y > HEIGHT - 10) {
         return;
     }
-    point.x = velocity.x * time + X0
-    point.y = 0.5 * G * time * time + velocity.y * time + Y0
+    particle.position.x = particle.velocity.x * time + particle.start.x
+    particle.position.y = 0.5 * G * time * time + particle.velocity.y * time + particle.start.y
 }
 
+const create_particle = (start, end, time) => {
+    const s = copy_point(start)
+    const e = copy_point(end)
+    return {
+        'start': s,
+        'position': copy_point(start),
+        'velocity': compute_velocity(s, e, time),
+        'time': time
+    }
+}
+
+const draw_particle = (particle, color) => {
+    draw_rectangle(particle.position.x, particle.position.y, 3, 3, color)
+}
+
+const update_and_draw_particles = (particles, time) => {
+    for (let i=0; i<particles.length; i++) {
+        const t = particles[i].time
+        if (time <= t) {
+            continue;
+        }
+        update_particle(particles[i], time - t)
+        draw_particle(particles[i], color)
+    }
+}
 
 let target_points = SMILEY
 let color = "yellow"
@@ -31,6 +54,8 @@ const params = new URLSearchParams(query)
 const svg = params.get('svg')
 if (svg == 'google') {
     target_points = GOOGLE
+} else if (svg == 'thanks') {
+    target_points = THANKS
 } else if (svg == 'paloma') {
     target_points = HEART
     color = "pink"
@@ -40,25 +65,19 @@ if (params.get('shuffle')) {
     target_points.sort(() => (Math.random() > 0.5) ? 1 : -1)
 }
 
-const points = target_points.map(_ => create_point(X0, Y0))
-const velocities = target_points.map((p, i) => compute_velocity(i * DELTA_TIME, p.x, p.y))
+const half = Math.floor(target_points.length / 2)
+const bottom_left = create_point(GAP + 400, HEIGHT - GAP)
+const bottom_right = create_point(WIDTH - GAP - 400, HEIGHT - GAP)
+const particles1 = target_points.filter((p, i) => i < half).map((p, i) => create_particle(bottom_left, p, i * DELTA_TIME))
+const particles2 = target_points.filter((p, i) => i >= half).map((p, i) => create_particle(bottom_right, p, i * DELTA_TIME))
 
 let t = 0
 
 const update = () => {
     clear_canvas()
-
-    for (let i=0; i<points.length; i++) {
-        const time = i * DELTA_TIME
-        if (t <= time) {
-            continue;
-        }
-        update_point(t - time, points[i], velocities[i])
-        draw_rectangle(points[i].x, points[i].y, 3, 3, color)
-    }
-
+    update_and_draw_particles(particles1, t)
+    update_and_draw_particles(particles2, t)
     t += 0.3
-
     requestAnimationFrame(update)
 }
 
